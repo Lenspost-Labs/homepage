a2ba70307828853ec4b540c2782a3fc96ba948f8
MrPreet <josonpreet@gmail.com>
2024-03-19 21:03:48 +0530

    fixed username overflow, used next/image

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 interface Props {
@@ -11,9 +12,12 @@ interface Props {
 function PageHeader({ backgroundImage, title, isCollection, isFeatured, isUser }: Props) {
 	return (
 		<>
-			<div className="flex items-start flex-col h-60 lg:h-96 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
+			<div className="flex items-start flex-col relative overflow-hidden z-0 h-60 lg:h-96 bg-cover bg-center bg-no-repeat">
+				<div className="absolute top-0 left-0 h-60 lg:h-96 w-full">
+					<Image src={backgroundImage} alt="background" fill objectFit="cover" />
+				</div>
 				{isCollection && (
-					<div className="flex flex-col items-start space-y-3 lg:space-y-5 w-full justify-end p-5 lg:p-20 h-full text-center">
+					<div className="flex flex-col items-start space-y-3 lg:space-y-5 w-full justify-end relative z-10 p-5 lg:p-20 h-full text-center">
 						{isFeatured && (
 							<p className="text-xs lg:text-base font-light text-white/75 uppercase px-2 py-1 lg:px-3 lg:py-1.5 rounded-full border border-white/75">
 								Featured This Week
--- src/components/collections/CollectionItem.tsx
+++ src/components/collections/CollectionItem.tsx
@@ -7,15 +7,24 @@ import { TbArrowFork } from 'react-icons/tb'
 import UserAvatar from '../UserAvatar'
 import { FaRegThumbsUp } from 'react-icons/fa'
 import PopoverMenu from '@/ui/Popover'
+import Image from 'next/image'
 
 function CollectionItem({ item }: any) {
 	const [showOverlay, setShowOverlay] = React.useState(false)
 	return (
 		<>
 			<div className="relative border-2 lg:border-4 h-min w-full group bg-theme-light-purple-50 border-theme-light-purple-50 p-1 lg:p-2 rounded-2xl">
-				<img src={item.urls.regular} alt={item.alt_description} className="rounded-xl object-cover w-full" />
+				<Image
+					src={item.urls.regular}
+					alt={item.alt_description}
+					width={item?.width}
+					height={item?.height}
+					quality={80}
+					sizes="100vw"
+					className="rounded-xl object-cover w-full"
+				/>
 				<div
-					className={cn('absolute inset-0 group-hover:opacity-100 opacity-0 duration-100 bg-black/25 p-3 m-1 lg:m-2 rounded-xl', {
+					className={cn('absolute inset-0 group-hover:opacity-100 opacity-100 duration-100 bg-black/25 p-3 m-1 lg:m-2 rounded-xl', {
 						'opacity-100': showOverlay,
 					})}
 				>
@@ -25,7 +34,7 @@ function CollectionItem({ item }: any) {
 								<TbArrowFork className="lg:w-5 lg:h-5 w-3 h-3" />
 							</button>
 						</div>
-						<div className="flex flex-row items-center space-x-1">
+						<div className="flex flex-row items-center justify-center space-x-1">
 							<div className="lg:flex hidden flex-row items-center space-x-2 backdrop-blur-sm bg-white/25 rounded-full px-3 py-2">
 								<button className="text-white">
 									<LuRefreshCw className="lg:w-5 lg:h-5 w-3 h-3" />
@@ -39,7 +48,7 @@ function CollectionItem({ item }: any) {
 									position="right"
 									trigger={
 										<button>
-											<BsThreeDotsVertical className="lg:w-5 lg:h-5 w-4 h-4" color="white" />
+											<BsThreeDotsVertical className="lg:w-6 mt-2 lg:h-6 w-4 h-4" color="white" />
 										</button>
 									}
 									options={[
@@ -51,7 +60,7 @@ function CollectionItem({ item }: any) {
 						</div>
 					</div>
 					<div className="flex flex-row w-full absolute bottom-0 left-0 pb-3 justify-between items-center space-x-0">
-						<div className="px-3 max-w-64">
+						<div className="px-3 xl:max-w-[60%] lg:max-w-[60%] 2xl:max-w-[70%]">
 							<UserAvatar isVerified={true} username={item.user?.username} href="/profile/clayton" size="xs" />
 						</div>
 						<div className="px-3">
