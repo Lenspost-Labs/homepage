209fb6d5e66dd6609793ae2be5018f5939a6e80a
MrPreet <josonpreet@gmail.com>
2024-03-21 01:04:08 +0530

    mobile bugs

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,13 +4,13 @@ const Counter = ({ percentage, week }: { percentage: string; week: string }) => 
 	return (
 		<>
 			<div className="flex flex-col items-start space-y-4 relative z-10">
-				<div className="flex flex-row -mt-2 -mr-2 lg:-mt-5 lg:-mr-5 items-center justify-between border bg-white/80 backdrop-blur border-theme-light-purple rounded-[30px] px-1 lg:px-2 space-x-1 lg:space-x-2 py-1">
+				<div className="flex flex-row -mt-1 sm:-mt-2 -mr-2 lg:-mt-5 lg:-mr-5 items-center justify-between border bg-white/80 backdrop-blur border-theme-light-purple rounded-[30px] px-1 lg:px-2 space-x-1 lg:space-x-2 py-1">
 					<div>
 						<ArrowUp className="text-theme-purple w-4 h-4" />
 					</div>
 					<div className="flex flex-row items-center space-x-1">
-						<p className="text-sm lg:text-base text-theme-purple">{percentage}%</p>
-						<p className="text-sm lg:text-base text-theme-gray">{week} week</p>
+						<p className="text-xs sm:text-sm lg:text-base text-theme-purple">{percentage}%</p>
+						<p className="text-xs sm:text-sm lg:text-base text-theme-gray">{week} week</p>
 					</div>
 				</div>
 			</div>
--- src/components/TextCounter.tsx
+++ src/components/TextCounter.tsx
@@ -2,8 +2,8 @@ const TextCounter = ({ title, count }: { title: string; count: string }) => {
 	return (
 		<>
 			<div className="flex flex-col items-start space-y-0 lg:space-y-2">
-				<p className="text-xl lg:text-2xl font-bold">{title}</p>
-				<p className="text-2xl lg:text-5xl font-[800] gradient-text gradient-text drop-shadow-[0px_4px_11px_rgba(61,11,56,0.13)]">{count}</p>
+				<p className="text-lg sm:text-xl lg:text-2xl font-bold">{title}</p>
+				<p className="text-xl sm:text-2xl lg:text-5xl font-[800] gradient-text gradient-text drop-shadow-[0px_4px_11px_rgba(61,11,56,0.13)]">{count}</p>
 			</div>
 		</>
 	)
