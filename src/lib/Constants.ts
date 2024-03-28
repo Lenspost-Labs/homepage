e8b047b9769dfc7566483413d9bd3581f47109d5
codescalper <msfunbook@gmail.com>
2024-03-28 15:32:21 +0530

    Removed collectibles from tab

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,6 +1,6 @@
 export const UNSPLASH_API_CLIENT_ID = '5ZMoF_dsZVyd50FgnndkBRfWkK-m5t-3mVAJgNym3Ro';
 
-export const tabs = ['All', 'Collectibles', 'Remix', 'NFTs', 'Backgrounds', 'Templates', 'Stickers']
+export const tabs = ['All', 'Remix', 'NFTs', 'Backgrounds', 'Templates', 'Stickers']
 export const nonBgRoutes = ['/raveshop', '/profile']
 export const MenuItems = [
   {
