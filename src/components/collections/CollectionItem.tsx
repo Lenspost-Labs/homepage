96e019076a0bdc2c58b117aafafef94f181294cd
codescalper <msfunbook@gmail.com>
2024-04-09 22:22:01 +0530

    feat: added warpcast link

--- src/components/collections/CollectionItem.tsx
+++ src/components/collections/CollectionItem.tsx
@@ -261,7 +261,7 @@ function CollectionItem({ item, username, tab }: any) {
 
   return (
     <>
-      {tab !== 'Remix' || (item.ipfsLink && item.ipfsLink.length > 0) ? (
+      {tab == 'Remix' && (item.ipfsLink && item.ipfsLink.length > 0) && (
         <Link  href={`https://app.poster.fun/?slugId=${item?.slug?.[0] ?? ''}`} target='blank'>
         <motion.div
           initial={{ opacity: 0, y: 200 }}
@@ -328,7 +328,76 @@ function CollectionItem({ item, username, tab }: any) {
           </div>
         </motion.div>
         </Link>
-      ) : null}
+      )}
+      {tab === 'Chicken' && item.platform === 'farcaster' && (
+          <Link  href={`https://warpcast.com/~/conversations/${item?.txHash}`} target='blank'>
+          <motion.div
+            initial={{ opacity: 0, y: 200 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              type: 'spring',
+              stiffness: 200,
+              damping: 24,
+            }}
+            className="relative border-2 lg:border-4 w-full group bg-theme-light-purple-50 border-theme-light-purple-50 p-1 lg:p-2 rounded-2xl"
+          >
+            {renderImage()}
+            <div
+              className={cn('absolute inset-0 group-hover:opacity-100 opacity-0 duration-100 bg-black/25 p-3 m-1 lg:m-2 rounded-xl', {
+                'opacity-100': showOverlay,
+              })}
+            >
+              <div className="flex flex-row items-center justify-between">
+                {/* <div className="flex flex-row items-center lg:space-x-2 backdrop-blur-sm bg-white/25 rounded-full px-2 py-2">
+                  <button className="text-white">
+                    <TbArrowFork className="lg:w-5 lg:h-5 w-3 h-3" />
+                  </button>
+                </div> */}
+                <div className="flex flex-row items-center justify-center space-x-1">
+                  {/* <div className="lg:flex hidden flex-row items-center space-x-2 backdrop-blur-sm bg-white/25 rounded-full px-3 py-2">
+                    <button className="text-white">
+                      <LuRefreshCw className="lg:w-5 lg:h-5 w-3 h-3" />
+                    </button>
+                    <div>
+                      <p className="text-white text-base font-medium">{item.likes}k</p>
+                    </div>
+                  </div> */}
+                  {/* <div>
+                    <PopoverMenu
+                      position="right"
+                      trigger={
+                        <button>
+                          <BsThreeDotsVertical className="lg:w-6 mt-2 lg:h-6 w-4 h-4" color="white" />
+                        </button>
+                      }
+                      options={[
+                        { label: 'Share', onClick: () => console.log('Trending') },
+                        { label: 'Embed', onClick: () => console.log('Newest') },
+                      ]}
+                    />
+                  </div> */}
+                </div>
+              </div>
+              <div className="flex flex-row w-full absolute bottom-0 left-0 pb-3 justify-between items-center space-x-0">
+                <div className="px-3 max-w-auto xl:max-w-[60%] lg:max-w-[60%] 2xl:max-w-[70%]">
+                  <UserAvatar isVerified={true} username={item.ownerId} href={`/profile/${item.ownerId}`} size="xs" />
+                </div>
+                <div className="px-3">
+                  {/* <div className="flex flex-row items-center space-x-2 backdrop-blur-sm bg-white/25 rounded-full px-3 py-2">
+                    <button className="text-white">
+                      <FaRegThumbsUp className="lg:w-5 lg:h-5 w-3 h-3" />
+                    </button>
+                    <button className="text-white">
+                      <LuRefreshCw className="lg:w-5 lg:h-5 w-3 h-3" />
+                    </button>
+                  </div> */}
+                </div>
+              </div>
+            </div>
+          </motion.div>
+          </Link>
+
+      )}
     </>
   )
 }
